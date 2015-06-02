91ad658141b4e02738a0b7a2c39fa4ce16392b32
Steve Johnson <steve@graffitilabs.com>
2015-06-01 17:15:27 -0700

    Rect test/render

--- src/renderer/canvasRenderer.js
+++ src/renderer/canvasRenderer.js
@@ -6,18 +6,40 @@ var canvasRendererNamespace = {
 
 
 // registerCanvasRenderer('Ellipse', function(shape, ctx))
-let registerCanvasRenderer = canvasRendererNamespace.registerCanvasRenderer =
+var registerCanvasRenderer = canvasRendererNamespace.registerCanvasRenderer =
   function(typeId, renderShape) {
     canvasRendererNamespace.registry[typeId] = renderShape;
   };
 
 
 canvasRendererNamespace.renderShapeToCanvas = function(shape, ctx) {
-  if (!canvasRendererNamespace.registry[typeId]) {
-    throw ("There is no renderer for " + typeId)
+  if (!canvasRendererNamespace.registry[shape.typeId]) {
+    throw ("There is no renderer for " + shape.typeId)
   }
-  canvasRendererNamespace.registry[typeId](shape, ctx)
+  canvasRendererNamespace.registry[shape.typeId](shape, ctx)
 }
+
+registerCanvasRenderer('Rectangle', function(shape, ctx) {
+  var s = shape.attributes.style;
+  var d = shape.attributes.dimensions;
+
+  if (s.fillColor) {
+    ctx.fillStyle = s.fillColor;
+    ctx.fillRect(d.xmin, d.ymin, d.xmax - d.xmin, d.ymax - d.ymin);
+  }
+
+  if (s.strokeColor && s.strokeWidth) {
+    ctx.lineWidth = s.strokeWidth;
+    ctx.strokeStyle = s.strokeColor;
+    if (s.strokeWidth % 2 == 0) {
+      ctx.strokeRect(d.xmin, d.ymin, d.xmax - d.xmin, d.ymax - d.ymin);
+    } else {
+      // fix single-pixel offset
+      ctx.strokeRect(
+        d.xmin + 0.5, d.ymin + 0.5, d.xmax - d.xmin, d.ymax - d.ymin);
+    }
+  }
+})
 
 
 module.exports = canvasRendererNamespace;
--- test_html/runTests.js
+++ test_html/runTests.js
@@ -1,4 +1,41 @@
 "use strict";
 
-var rect = LC.shape.createShape(
-  'Rectangle', {xmin: 0, ymin: 0, xmax: 100, ymax: 100});
+// callback = function(div, ctx)
+var withCanvas = function(w, h, heading, desc, callback) {
+  var div = document.createElement('div');
+  div.className = 'canvas-test';
+  div.innerHTML = '<h2>' + heading + '</h2>' + desc;
+
+  var canvas = document.createElement('canvas');
+  canvas.width = w;
+  canvas.height = h;
+  canvas.style.width = w;
+  canvas.style.height = h;
+
+  div.appendChild(canvas);
+  document.body.appendChild(div);
+
+  var ctx = canvas.getContext('2d');
+  callback(ctx);
+}
+
+
+var renderShapes = function(w, h, label, shapes) {
+  var desc = '<pre>' + shapes.map(function(s) {
+    return JSON.stringify(s, null, 2);
+  }).join('\n') + '</pre>';
+  withCanvas(w, h, label, desc, function(ctx) {
+    for (var i in shapes) {
+      LC.renderer.canvasRenderer.renderShapeToCanvas(shapes[i], ctx);
+    }
+  });
+}
+
+
+renderShapes(120, 120, 'Basic rect', [
+  LC.shape.createShape(
+    'Rectangle', {
+      dimensions: {xmin: 10, ymin: 10, xmax: 110, ymax: 110},
+      style: {strokeColor: 'black', strokeWidth: 1, fillColor: 'red'}
+    })
+]);
